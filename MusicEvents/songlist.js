6784f00ee53c567d6d8d3ceeee7512a8e7ee1753
llanaRosemie <117790015+llanaRosemie@users.noreply.github.com>
2023-05-16 21:00:00 +0800

    Add files via upload

---
+++ MusicEvents/songlist.js
@@ -0,0 +1,64 @@
+// // To add more song, just copy the following code and paste inside the array
+
+// //   {
+// //     name: "Here is the music name",
+// //     artist: "Here is the artist name",
+// //     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+// //     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+// //   }
+
+// //paste it inside the array as more as you want music then you don't need to do any other thing
+
+// let allMusic = [
+//     {
+//         img : 'images/starving.jpg',
+//         name : 'Starving',
+//         artist : 'Hailee Steinfeld',
+//         music : 'music/Starving.mp3'
+//     },
+//     {
+//         img : 'images/Stay.png',
+//         name : 'Stay',
+//         artist : 'The Kid LAROI, Justin Bieber',
+//         music : 'music/Stay.mp3'
+//     },
+    
+//     {
+//         img : 'images/leanon.jpg',
+//         name : 'Lean On',
+//         artist : 'Major Lazer & DJ Snake (feat. MØ) ',
+//         music : 'music/Lean On.mp3'
+//     },
+//     {
+//         img : 'images/HDIYL.jpg',
+//         name : 'How Deep Is Your Love',
+//         artist : 'Calvin Harris & Disciples',
+//         music : 'music/How Deep Is Your Love.mp3'
+//     },
+//     {
+//         img : 'images/WhatYourWaitingFor.jpg',
+//         name : 'What Your Waiting For',
+//         artist : 'SOMI',
+//         music : 'music/SOMI_(_What_You_Waiting_For.mp3'
+//     },
+//     {
+//         img : 'images/sohot.jpg',
+//         name : 'So Hot',
+//         artist : 'BlackPink',
+//         music : 'music/BLACKPINK_SO_HOT.mp3'
+//     },
+//     {
+//         img : 'images/StayWithMe.jpg',
+//         name : 'Stay With Me',
+//         artist : 'Miki Matsubara',
+//         music : 'music/stay With Me.mp3'
+//     }
+
+//   // like this paste it and remember to give comma after ending of this bracket }
+//   // {
+//   //   name: "Here is the music name",
+//   //   artist: "Here is the artist name",
+//   //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//   //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   // }
+// ];
